f27385b5d33043c4413937f33f0b458be8d230a3
Anders Brujordet <anders@brujordet.no>
2010-12-06 15:30:22 +0100

    Trying to hack a filechooser, seems like i have to create on manually :/

--- Tuxility/src/com/grimmvarg/android/tuxility/KernelHacking.java
+++ Tuxility/src/com/grimmvarg/android/tuxility/KernelHacking.java
@@ -1,10 +1,15 @@
 package com.grimmvarg.android.tuxility;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
 
 public class KernelHacking extends Activity implements OnClickListener{
 	private TuxHelper tuxHelper = TuxHelper.getInstance();
